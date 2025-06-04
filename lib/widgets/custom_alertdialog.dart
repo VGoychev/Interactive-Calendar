7bb651ff3f082f0072a21dd2a3be54d6bf4e57c5
VGoychev <vladimirgoichev@gmail.com>
2025-06-04 14:26:56 +0300

    Changed the code structure for better readability of the custom alertdialog as it got lot of code in it.

--- lib/widgets/custom_alertdialog.dart
+++ lib/widgets/custom_alertdialog.dart
@@ -8,173 +8,166 @@ class CustomAlertDialog extends StatelessWidget {
   final EventController<Object?> eventController;
   final CalendarEventData<Object?> event;
   final String uid;
-  const CustomAlertDialog(
-      {super.key,
-      required this.event,
-      required this.uid,
-      required this.eventController});
+
+  const CustomAlertDialog({
+    super.key,
+    required this.event,
+    required this.uid,
+    required this.eventController,
+  });
 
   @override
   Widget build(BuildContext context) {
     final CalendarEvent? realEvent = event.event as CalendarEvent?;
+    final String? description = event.description?.trim();
+    final bool hasDescription = description != null && description.isNotEmpty;
 
     return AlertDialog(
       titlePadding: const EdgeInsets.fromLTRB(18, 8, 18, 0),
       contentPadding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
       actionsPadding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
-      title: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const Text(
-                  "Event Details",
-                  style: TextStyle(fontSize: 16),
-                  textAlign: TextAlign.start,
-                ),
-                TextButton(
-                  onPressed: () => print("Happy"),
-                  child: Text('Edit'),
-                  style: TextButton.styleFrom(
-                    foregroundColor: Theme.of(context).colorScheme.primary,
-                    textStyle: const TextStyle(fontSize: 14),
-                  ),
-                )
-              ]),
-          const Divider(
-            thickness: 0.5,
-            height: 0.5,
-          ),
-        ],
-      ),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 2),
-            child: Text(
-              "${event.title}",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Text(
-            "${DateFormat('MMMM d, y').format(event.date)},",
-            style: TextStyle(
-                fontSize: 12,
-                color:
-                    Theme.of(context).colorScheme.onSecondary.withOpacity(0.6)),
-          ),
-          Text(
-            "from ${DateFormat('HH:mm').format(realEvent?.startTime ?? event.startTime!)} "
-            "to ${DateFormat('HH:mm').format(realEvent?.endTime ?? event.endTime!)}",
-            style: TextStyle(
-                fontSize: 12,
-                color:
-                    Theme.of(context).colorScheme.onSecondary.withOpacity(0.6)),
-          ),
-          if (event.description != null && event.description!.trim().isNotEmpty)
-            SizedBox(
-              height: 6,
-            ),
-          if (event.description != null && event.description!.trim().isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(top: 6),
-              child: Text(
-                "${event.description}",
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-            ),
-        ],
-      ),
-      actions: [
-        Stack(
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: TextButton(
-                child: const Text(
-                  "Delete Event",
-                  style: TextStyle(color: Colors.red),
-                ),
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.vertical(top: Radius.circular(16)),
-                    ),
-                    builder: (sheetContext) {
-                      final screenWidth = MediaQuery.of(context).size.width;
+      title: _buildTitle(context),
+      content: _buildContent(context, realEvent, hasDescription, description),
+      actions: [_buildActions(context)],
+    );
+  }
 
-                      return SizedBox(
-                        width: screenWidth,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Text(
-                                'Are you sure you want to delete this event?',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 20),
-                              Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  SizedBox(
-                                    width: double.infinity,
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: Colors.red,
-                                      ),
-                                      child: const Text('Delete'),
-                                      onPressed: () async {
-                                        Navigator.of(sheetContext).pop();
-                                        await deleteCalendarEvent(
-                                          event: event,
-                                          controller: eventController,
-                                          uid: uid,
-                                        );
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: double.infinity,
-                                    child: TextButton(
-                                      child: const Text('Cancel'),
-                                      onPressed: () =>
-                                          Navigator.of(sheetContext).pop(),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
+  Widget _buildTitle(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Event Details", style: TextStyle(fontSize: 16)),
+            TextButton(
+              onPressed: () => print("Happy"),
+              style: TextButton.styleFrom(
+                foregroundColor: Theme.of(context).colorScheme.primary,
+                textStyle: const TextStyle(fontSize: 14),
               ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                child: const Text("Close"),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-            ),
+              child: const Text('Edit'),
+            )
           ],
         ),
+        const Divider(thickness: 0.5, height: 0.5),
       ],
+    );
+  }
+
+  Widget _buildContent(BuildContext context, CalendarEvent? realEvent,
+      bool hasDescription, String? description) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "${event.title}",
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "${DateFormat('MMMM d, y').format(event.date)},",
+          style: TextStyle(
+            fontSize: 12,
+            color: Theme.of(context).colorScheme.onSecondary.withOpacity(0.6),
+          ),
+        ),
+        Text(
+          "from ${DateFormat('HH:mm').format(realEvent?.startTime ?? event.startTime!)} "
+          "to ${DateFormat('HH:mm').format(realEvent?.endTime ?? event.endTime!)}",
+          style: TextStyle(
+            fontSize: 12,
+            color: Theme.of(context).colorScheme.onSecondary.withOpacity(0.6),
+          ),
+        ),
+        if (hasDescription) const SizedBox(height: 6),
+        if (hasDescription)
+          Text(
+            description!,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+      ],
+    );
+  }
+
+  Widget _buildActions(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: TextButton(
+            onPressed: () => _showDeleteConfirmation(context),
+            child:
+                const Text("Delete Event", style: TextStyle(color: Colors.red)),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Close"),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _showDeleteConfirmation(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (sheetContext) {
+        final screenWidth = MediaQuery.of(context).size.width;
+        return SizedBox(
+          width: screenWidth,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Are you sure you want to delete this event?',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                        onPressed: () async {
+                          Navigator.of(sheetContext).pop();
+                          await deleteCalendarEvent(
+                            event: event,
+                            controller: eventController,
+                            uid: uid,
+                          );
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Delete'),
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: TextButton(
+                        onPressed: () => Navigator.of(sheetContext).pop(),
+                        child: const Text('Cancel',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
